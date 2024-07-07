84c29a15a920d24f9b21648dc1afc32c61c41a0e
GjoeKariuki <githaigageorge96@gmail.com>
2024-07-07 14:00:40 +0300

    journal url,model & views

--- journals/urls.py
+++ journals/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
-from knox import views as knox_views
-from users.views import Registration, Login
-from users.views import user_list,user_detail,change_password
+from journals.views import journal_detail,journal_list
 
 
 
 
 
 urlpatterns = [
-    path('register/',Registration.as_view()),
-    path('login/', Login.as_view()),
-    path('logout/',knox_views.LogoutView.as_view()),
-    path('user/', user_list),
-    path('user/<str:pk>/',user_detail),
-    path('change-password', change_password),
+        path('journal_list/', journal_list,name="journals"),
+        path('journal_detail/<str:pk>/',journal_detail, name="journaldetails")
 
 ]
+
--- journals/views.py
+++ journals/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from knox.auth import TokenAuthentication
+from rest_framework.response import Response
+
+from journals.models import JournalEntry
+from journals.dbhelper import MyDBCRUDHelper
+from journals.serializers import JournalEntrySerializer,GetJournalEntriesSerializer
+from journals.filters import JournalEntryFilter
 
 # Create your views here.
+
+
+@api_view(['GET','POST'])
+@authentication_classes([BasicAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def journal_list(request):
+    mydbhelper = MyDBCRUDHelper(myrequest=request, 
+                mymodel=JournalEntry, 
+                mymodelfilter=JournalEntryFilter, 
+                getterSerializer=GetJournalEntriesSerializer,
+                mymodelserializer=JournalEntrySerializer)
+    if request.method == 'GET':
+        return mydbhelper.list_objects()
+    elif request.method == 'POST':
+        return mydbhelper.create_object()
+    
+
+
+@api_view(['GET','PUT','DELETE'])
+@authentication_classes([BasicAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def journal_detail(request,pk):
+    mydbhelper = MyDBCRUDHelper(myrequest=request, 
+                mymodel=JournalEntry, 
+                mymodelfilter=JournalEntryFilter, 
+                mymodelserializer=JournalEntrySerializer,
+                mypk=pk,
+                getterSerializer=GetJournalEntriesSerializer
+                )
+
+    if request.method == 'GET':
+        return mydbhelper.list_object()
+    elif request.method == 'PUT':
+        return mydbhelper.update_object()
+    elif request.method == 'DELETE':
+        return mydbhelper.delete_object()
